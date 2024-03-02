7071c4a7c4d26242e7888c3e14ac317c4f7dfee4
YaBoiPercy <iperseusftaylor@gmail.com>
2024-03-01 19:29:49 -0500

    Map system update
    Created a function that automatically disables the map when the object the script is attached to is enabled. Seems to work for both dialogue choices AND regular dialogue. Theoretically works for ANY object. it's called scr_DialogueDisable. You will need to manually point it at the btn_map object I made, and any other buttons you want to disable.

---
+++ FirstPrototype/Assets/scr_DialogueDisable.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_DialogueDisable : MonoBehaviour
+{
+    //public GameObject UI;
+    //public GameObject UI_Dialogue;
+    //public GameObject UI_Menu;
+    public GameObject[] buttons;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    //UI = GameObject.FindGameObjectWithTag("Dialogue");
+    //}
+    private void OnEnable()
+    {
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(false);
+        }
+    }
+    private void OnDisable()
+    {
+        {
+            foreach (GameObject button in buttons)
+            {
+                button.SetActive(true);
+            }
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(false);
+        }
+        //    UI = GameObject.FindGameObjectWithTag("Dialogue");
+        //    if (UI == null)
+        //    {
+        //        print("NULL");
+        //    }
+        //    if (UI.activeSelf) 
+        //    {   foreach (GameObject button in buttons)
+        //        {
+        //            button.SetActive(false);
+        //        }
+        //    }
+        //    else if (UI == null) 
+        //    {
+        //        foreach (GameObject button in buttons)
+        //        {
+        //            button.SetActive(true);
+        //        }
+        //    }
+    }
+    }
