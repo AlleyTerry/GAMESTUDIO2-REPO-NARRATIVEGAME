f557901f402325d84fdd2824bd4152b3281833fb
AlleyTerry <art8689@nyu.edu>
2024-04-01 22:59:12 -0400

    enum stuff started
    eh it kinda works

--- FirstPrototype/Assets/Scripts/EndDay.cs
+++ FirstPrototype/Assets/Scripts/EndDay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,18 @@ public class EndDay : MonoBehaviour
     //set enmum days
     //make switch statement and cases for ending each day
     public static EndDay instance;
+    
+
+    public enum Days
+    {
+        Day1Test,
+        Day2Test,
+        Day3Test,
+        Day4Test,
+        Day5Test
+    }
+
+    public Days CurrentDay = Days.Day1Test;
 
     private void Awake()
     {
@@ -24,10 +37,33 @@ public class EndDay : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        SceneManager.LoadScene("Day1Test", LoadSceneMode.Additive);
+    }
+
     public void EndDayButton()
     {
-        SceneManager.LoadScene("Day2Test");
-        MainFlowChart.ExecuteBlock("Day2START");
+        switch (CurrentDay)
+        {
+            case Days.Day1Test:
+                SceneManager.LoadScene("Day2Test");
+                MainFlowChart.ExecuteBlock("Day2START");
+                break;
+            case Days.Day2Test:
+                SceneManager.LoadScene("Day3Test");
+                MainFlowChart.ExecuteBlock("Day3START");
+                break;
+            case Days.Day3Test:
+                SceneManager.LoadScene("Day4Test");
+                MainFlowChart.ExecuteBlock("Day4START");
+                break;
+            case Days.Day4Test:
+                SceneManager.LoadScene("Day5Test");
+                MainFlowChart.ExecuteBlock("Day5START");
+                break;
+        }
+        
 
     }
 }
